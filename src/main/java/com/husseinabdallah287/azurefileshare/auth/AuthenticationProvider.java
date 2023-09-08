d0ec4d1d0e04c7b6235d69396f52790b8661f218
mishobo <husseinabdalla287@gmail.com>
2023-09-08 10:38:42 +0300

    providerCode and invoice name parameters

--- src/main/java/com/husseinabdallah287/azurefileshare/auth/AuthenticationProvider.java
+++ src/main/java/com/husseinabdallah287/azurefileshare/auth/AuthenticationProvider.java
@@ -1,27 +1,20 @@
 package com.husseinabdallah287.azurefileshare.auth;
 
-import com.husseinabdallah287.azurefileshare.fileUpload.FileUploadUtil;
 import com.husseinabdallah287.azurefileshare.model.KenGenDrives;
 import com.husseinabdallah287.azurefileshare.model.KenGenToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
-import static org.apache.logging.log4j.message.ParameterizedMessage.format;
 
 @Service
 public class AuthenticationProvider {
@@ -40,21 +33,7 @@ public class AuthenticationProvider {
 
     private List<String> scopes = Arrays.asList(scope);
 
-
-
-//    public IGraphServiceClient getAuthProvider() {
-//        IAuthenticationProvider mAuthenticationProvider;
-//
-//        try {
-//            //String accessToken = getAuthToken();
-//        } catch (Exception e) {
-//            throw new Error("Could not create a graph client: " + e.getLocalizedMessage());
-//        }
-//
-//    }
-
-    public void getAuthToken(String filePath) throws IOException {
-
+    public KenGenToken getKenGenToken(){
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.add("grant_type", "client_credentials");
         formData.add("client_id", "69657438-450f-4961-8c7e-f418a59a5077"); // client_secret
@@ -73,6 +52,10 @@ public class AuthenticationProvider {
 
         System.out.println(token.getAccess_token());
 
+        return token;
+    }
+
+    public KenGenDrives getKenGenSharepointDrives(KenGenToken token){
 
         KenGenDrives drives = WebClient.create()
                 .get()
@@ -90,19 +73,27 @@ public class AuthenticationProvider {
                         System.out.println(value.getId()
                         ));
 
-        System.out.println("filePath :" + filePath);
+        return drives;
+    }
 
+    public void uploadFilesTokenGenSharePoint(String filePath, String providerCode, String invoiceName) throws IOException {
+        var token = getKenGenToken();
+        var drives = getKenGenSharepointDrives(token);
+
+        System.out.println("filePath :" + filePath);
         var bytes = new FileSystemResource((filePath)).getInputStream()
                 .readAllBytes();
 
-//        MultipartBodyBuilder builder = new MultipartBodyBuilder();
-//        builder.part("file", multipartFile.getResource());
-//
+
         String uploadFile = WebClient.create()
                 .put()
                 .uri("https://graph.microsoft.com/v1.0/drives/" +
                         "b!enjU1lkSG0aytcjBa72uMThP1IrLvyJCjpIof8V62Zhw0vWJYTSKSYkx4sWhdDgt" +
-                        "/items/root:/LCT Medical Reports/007/sample.pdf:/content")
+                        "/items/root:/LCT Medical Reports/" +
+                        providerCode +
+                        "/" +
+                        invoiceName +
+                        ":/content")
                 .headers(httpHeaders -> httpHeaders.setBearerAuth(token.getAccess_token()))
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .bodyValue(bytes)
@@ -112,7 +103,32 @@ public class AuthenticationProvider {
                 .block();
 
         System.out.println(uploadFile);
+
     }
+
+
+//    public void getAuthToken(String filePath) throws IOException {
+//        System.out.println("filePath :" + filePath);
+//
+//        var bytes = new FileSystemResource((filePath)).getInputStream()
+//                .readAllBytes();
+//
+//
+//        String uploadFile = WebClient.create()
+//                .put()
+//                .uri("https://graph.microsoft.com/v1.0/drives/" +
+//                        "b!enjU1lkSG0aytcjBa72uMThP1IrLvyJCjpIof8V62Zhw0vWJYTSKSYkx4sWhdDgt" +
+//                        "/items/root:/LCT Medical Reports/007/sample.pdf:/content")
+//                .headers(httpHeaders -> httpHeaders.setBearerAuth(token.getAccess_token()))
+//                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+//                .bodyValue(bytes)
+//                .exchange()
+//                .block()
+//                .bodyToMono(String.class)
+//                .block();
+//
+//        System.out.println(uploadFile);
+//    }
 
 
 }
--- src/main/java/com/husseinabdallah287/azurefileshare/controller/UploadDocToSharePoint.java
+++ src/main/java/com/husseinabdallah287/azurefileshare/controller/UploadDocToSharePoint.java
@@ -3,7 +3,6 @@ package com.husseinabdallah287.azurefileshare.controller;
 import com.husseinabdallah287.azurefileshare.auth.AuthenticationProvider;
 import com.husseinabdallah287.azurefileshare.fileUpload.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,17 +18,13 @@ public class UploadDocToSharePoint {
     private AuthenticationProvider authenticationProvider;
 
 
-//    @RequestMapping(value = "/sharepoint/upload",method = RequestMethod.POST)
-//    public String uploadDocToSharePoint() throws Exception {
-//        authenticationProvider.setUploadSession();
-//
-//        return "Successfully uploaded file to sharepoint";
-//    }
-
-
-
     @PostMapping("/sharepoint/gtToken")
-    public void getMicrosoftAccess(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+    public void getMicrosoftAccess(
+            @RequestParam("file") MultipartFile multipartFile,
+            @RequestParam("providerCode") String providerCode,
+            @RequestParam("invoiceName") String invoiceName
+
+    ) throws IOException {
 
         String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
         long size = multipartFile.getSize();
@@ -37,7 +32,7 @@ public class UploadDocToSharePoint {
 
         System.out.println("filePath :" + filePath);
 
-        authenticationProvider.getAuthToken(filePath);
+        authenticationProvider.uploadFilesTokenGenSharePoint(filePath, providerCode, invoiceName);
     }
 
 }
